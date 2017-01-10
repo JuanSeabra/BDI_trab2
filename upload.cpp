5badedf618542d40879662e57ab8aa2f3b6256f5
Waldomiro Juan Gadelha Seabra <wjgs@icomp.ufam.edu.br>
2017-01-09 22:13:11 -0400

    upload fazendo a leitura normalmente

--- upload.cpp
+++ upload.cpp
@@ -5,12 +5,14 @@
 #include <fstream>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string.hpp>
 #include <vector>
 #include <boost/tokenizer.hpp>
 using boost::tokenizer;
 using boost::escaped_list_separator;
 using boost::split;
 using boost::is_any_of;
+using boost::erase_all;
 using namespace std;
 
 #include "artigo.h"
@@ -20,20 +22,8 @@ using namespace std;
 //     primary index file (B-Tree)
 //     secondary index file (B-Tree)
 
-void remove_all_chars(char* str, char c) {
-	char *pr = str, *pw = str;
-	while (*pr) {
-		*pw = *pr++;
-		pw += (*pw != c);
-	}
-	*pw = '\0';
-}
-
 vector<string> parse(string s){
-	char aux[4096];
-	strcpy(aux,s.c_str());
-	remove_all_chars(aux,'\\');
-	s = aux;
+	erase_all(s,"\\");
 	typedef tokenizer<escaped_list_separator<char> > so_tokenizer;
 	vector<string> strs;
 	so_tokenizer tok(s, escaped_list_separator<char>('\\', ';', '\"'));
@@ -48,11 +38,9 @@ int main(int argc, char *argv[]){
 	ifstream f;
 	FILE *out;
 	string linha,linha_aux;
-	char tok[1024];
 	Artigo registro;
 	int count = 0, bucket_count = 0;
 	vector<string> strs;
-	//HashBuckets hash_registros (out,"overflowFile",100);
 
 
 	cout << sizeof(Artigo) << endl;
@@ -63,51 +51,48 @@ int main(int argc, char *argv[]){
 	}
 
 	f.open(argv[1]);
-	out = fopen("Artigo.dat","w");
+	out = fopen("Artigo.dat","w+");
+
+	HashBuckets hash_registros (out,"overflowFile",145920);
 
 	while (getline(f,linha)) {
 		strs = parse(linha);
 
-		/*Correção de erro de quebra de linha*/
+		//Correção de erro de quebra de linha
 		if (strs.size() < 7){
 			getline(f,linha_aux);
 			linha += linha_aux;
 			strs = parse(linha);
 		}
 
-		strcpy(tok,strs[0].c_str());
-		registro.id = atoi(tok);
+		registro.id = stoi(strs[0]);
 
 
-		strcpy(tok,strs[1].c_str());
-		strncpy(registro.titulo,tok,300);
+		strncpy(registro.titulo,strs[1].c_str(),300);
 
-		strcpy(tok,strs[2].c_str());
-		registro.ano = (unsigned short) atoi(tok);
+		registro.ano = (unsigned short) stoi(strs[2]);
 
-		strcpy(tok,strs[3].c_str());
-		strncpy(registro.autores,tok,100);
+		strncpy(registro.autores,strs[3].c_str(),100);
 
-		strcpy(tok,strs[4].c_str());
-		registro.citacoes = (unsigned short) atoi(tok);
+		registro.citacoes = (unsigned) stoi(strs[4]);
 
-		strcpy(tok,strs[5].c_str());
-		strncpy(registro.atualizacao,tok,19);
+		strncpy(registro.atualizacao,strs[5].c_str(),19);
 
 
-		strcpy(tok,strs[6].c_str());
-		strncpy(registro.snippet,tok,100);
+		strncpy(registro.snippet,strs[6].c_str(),100);
 
-		/*Escreve arquivo de dados
-		 * Escreve arquivo de indice primario
-		 * Escreve arquivo de indice secundario*/
+		//Escreve arquivo de dados
+		//Escreve arquivo de indice primario
+		//Escreve arquivo de indice secundario
 		count++;
 		if (count%7 == 0) bucket_count++;
 		strs.clear();
+		hash_registros.insert(registro);
+		cout << "inseridos " << count << " registros até'agora!!" << endl;
 	}
 	f.close();
 	fclose(out);
-	cout << "Foram escritos " << count << " blocos!" << endl
+	cout << "Foram escritos " << count << " registros!" << endl
 		<< "numero de buckets " << bucket_count << endl;
 	return 0;
 }
