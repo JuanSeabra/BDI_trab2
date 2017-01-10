17f93bbabc18d4b3b5d417f9bb71b26b9374de95
Waldomiro Juan Gadelha Seabra <wjgs@icomp.ufam.edu.br>
2017-01-10 10:58:40 -0400

    hash corrigido

--- hashing.cpp
+++ hashing.cpp
@@ -3,7 +3,7 @@
 
 HashBuckets::HashBuckets(char *hashFileName, FILE *dataFile, char *overflowFileName, int num_bucketsTotais){
 	hashFile = fopen(hashFileName,"w+");
-	fread(bucket_ptrs,sizeof(int),sizeof(int)*num_bucketsTotais,hashFile);
+	fread(bucket_ptrs,sizeof(int),num_bucketsTotais,hashFile);
 	this->num_bucketsTotais = num_bucketsTotais;
 	this->dataFile = dataFile;
 	this->overflowFile = fopen(overflowFileName,"w+");
@@ -22,7 +22,7 @@ HashBuckets::HashBuckets(FILE *dataFile, char *overflowFileName, int num_buckets
 
 HashBuckets::~HashBuckets(){
 	fseek(hashFile,0,SEEK_SET);
-	fwrite(bucket_ptrs,sizeof(int),sizeof(int)*num_buckets,hashFile);
+	fwrite(bucket_ptrs,sizeof(int),num_buckets,hashFile);
 	fclose(hashFile);
 	fclose(overflowFile);
 	//delete bucket_ptrs;
@@ -58,50 +58,53 @@ int HashBuckets::insert(Artigo registro){
 		bucket_em_memoria = criaBucket();
 		bucket_em_memoria->bloco[0] = registro;
 		bucket_em_memoria->num_registros_ocupados++;
-		fwrite(bucket_em_memoria,sizeof(Bucket),sizeof(Bucket),dataFile);
+		fwrite(bucket_em_memoria,sizeof(Bucket),1,dataFile);
 		num_buckets++;
 		std::cout << "teste[1]" << std::endl;
 		delete bucket_em_memoria;
 		return 0;
 	}
+	bucket_em_memoria = new Bucket;
 	fseek(dataFile,bucket_ptrs[bucket_index],SEEK_SET);
-	fread(bucket_em_memoria,sizeof(Bucket),sizeof(Bucket),dataFile);
+	std::cout << ftell(dataFile) << std::endl;
+	fread(bucket_em_memoria,sizeof(Bucket),1,dataFile);
 	if(bucket_em_memoria->num_registros_ocupados < 7){
 		bucket_em_memoria->bloco[bucket_em_memoria->num_registros_ocupados] = registro;
 		bucket_em_memoria->num_registros_ocupados++;
 		fseek(dataFile,bucket_ptrs[bucket_index],SEEK_SET);
-		fwrite(bucket_em_memoria,sizeof(Bucket),sizeof(Bucket),dataFile);
+		fwrite(bucket_em_memoria,sizeof(Bucket),1,dataFile);
 		num_buckets++;
 		std::cout << "teste[2]" << std::endl;
-		//delete bucket_em_memoria;
+		delete bucket_em_memoria;
 		return 0;
 	}
 	if(bucket_em_memoria->ptr_arquivo_de_overflow == -1){
 		fseek(overflowFile,0,SEEK_END);
 		bucket_em_memoria->ptr_arquivo_de_overflow = ftell(overflowFile);
 		fseek(dataFile,bucket_ptrs[bucket_index],SEEK_SET);
-		fwrite(bucket_em_memoria,sizeof(Bucket),sizeof(Bucket),dataFile);
+		fwrite(bucket_em_memoria,sizeof(Bucket),1,dataFile);
 		overflow_em_memoria = criaOverflow();
 		overflow_em_memoria->bloco[0] = registro;
 		overflow_em_memoria->num_registros_ocupados++;
-		fwrite(overflow_em_memoria,sizeof(Overflow),sizeof(Overflow),overflowFile);
+		fwrite(overflow_em_memoria,sizeof(Overflow),1,overflowFile);
 		num_buckets++;
 		std::cout << "teste[3]" << std::endl;
-		//delete bucket_em_memoria;
-		//delete overflow_em_memoria;
+		delete bucket_em_memoria;
+		delete overflow_em_memoria;
 		return 1;
 	}
 	ptr_overflow_lido = bucket_em_memoria->ptr_arquivo_de_overflow;
+	overflow_em_memoria =  new Overflow;
 	while (ptr_overflow_lido != -1) {
 		fseek(overflowFile,ptr_overflow_lido,SEEK_SET);
-		fread(overflow_em_memoria,sizeof(Overflow),sizeof(Overflow),overflowFile);
+		fread(overflow_em_memoria,sizeof(Overflow),1,overflowFile);
 		if(overflow_em_memoria->num_registros_ocupados < 7){
 			overflow_em_memoria->bloco[overflow_em_memoria->num_registros_ocupados] = registro;
 			overflow_em_memoria->num_registros_ocupados++;
-			fwrite(overflow_em_memoria,sizeof(Overflow),sizeof(Overflow),overflowFile);
+			fwrite(overflow_em_memoria,sizeof(Overflow),1,overflowFile);
 			num_buckets++;
 			std::cout << "teste[4]" << std::endl;
-			//delete overflow_em_memoria;
+			delete overflow_em_memoria;
 			return 1;
 		}
 		ptr_overflow_anterior = ptr_overflow_lido;
@@ -109,15 +112,15 @@ int HashBuckets::insert(Artigo registro){
 		//delete overflow_em_memoria;
 	}
 	fseek(overflowFile,ptr_overflow_anterior,SEEK_SET);
-	fread(overflow_em_memoria,sizeof(Overflow),sizeof(Overflow),overflowFile);
+	fread(overflow_em_memoria,sizeof(Overflow),1,overflowFile);
 	fseek(overflowFile,0,SEEK_END);
 	overflow_em_memoria->ptr_prox = ftell(overflowFile);
 	overflow_em_memoria = criaOverflow();
 	overflow_em_memoria->bloco[0] = registro;
 	overflow_em_memoria->num_registros_ocupados++;
-	fwrite(overflow_em_memoria,sizeof(Overflow),sizeof(Overflow),overflowFile);
+	fwrite(overflow_em_memoria,sizeof(Overflow),1,overflowFile);
 	num_buckets++;
 	std::cout << "teste[5]" << std::endl;
-	//delete overflow_em_memoria;
+	delete overflow_em_memoria;
 	return 1;
 }
